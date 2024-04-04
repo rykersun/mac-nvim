21057b266b23dd1ea17056cbe82c489b9fda2992
Ryker Sun <rykersun2002@gmail.com>
2024-04-05 00:41:18 +0800

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
         configs.setup({
             -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-            ensure_installed = {"lua", "vim", "java"},
+            ensure_installed = {"lua", "vim", "java", "python"},
             sync_install = true, -- set this to true will avoid first time open error
             highlight = { enable = true },
             indent = { enable = true },  
