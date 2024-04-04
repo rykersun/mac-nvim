7cba67f7ec39a85dbc9c4570b1bd9bdfd8c18edf
Ryker Sun <rykersun2002@gmail.com>
2024-04-05 00:35:37 +0800

    update neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,7 +2,7 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     config = function()
         -- neotree settings
-        vim.keymap.set('n', '<leader>fs', ':Neotree<CR>')
+        vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>')
     end
 
 }
