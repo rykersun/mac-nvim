63427579363e4e9367ead5747ee6099499e9597e
Ryker Sun <rykersun2002@gmail.com>
2024-04-05 19:04:59 +0800

    disable theme

--- lua/vimsettings.lua
+++ lua/vimsettings.lua
@@ -3,7 +3,7 @@ vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
-vim.o.termguicolors = true
+-- vim.o.termguicolors = true
 
 -- set leader key
 vim.g.mapleader = " "
