93a923c72b4b7a261465d72208fbe77e72e02e37
Ryker Sun <rykersun2002@gmail.com>
2024-04-05 16:57:46 +0800

    update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,43 +1,44 @@
--- installed plugins
+-- installed plugins (Plugin list)
 return {
     { -- telescope
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        'nvim-telescope/telescope.nvim', tag = '0.1.6', -- a very powerful fuzzy file finder
           dependencies = { 'nvim-lua/plenary.nvim' }
     },
     { -- vscode dark theme
-        'Mofiqul/vscode.nvim'
+        'Mofiqul/vscode.nvim' -- theme
     },
     { -- treesitter
-        "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+        "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" -- make your code more colorful
     },
     { -- neotree
-        "nvim-neo-tree/neo-tree.nvim",
+        "nvim-neo-tree/neo-tree.nvim", -- file system explorer
         branch = "v3.x",
         dependencies = {
           "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons",
+          "nvim-tree/nvim-web-devicons", -- file icon
           "MunifTanjim/nui.nvim",
         }
     },
     { -- completions
-        'hrsh7th/nvim-cmp', dependencies = {
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-            'rafamadriz/friendly-snippets',
+        'hrsh7th/nvim-cmp', dependencies = { -- completion framwork
+            'L3MON4D3/LuaSnip', -- completion sources
+            'rafamadriz/friendly-snippets', -- another completion sources but gives vscode-like completions
+            'saadparwaiz1/cmp_luasnip', -- this plugin finished the completion
+            'hrsh7th/cmp-nvim-lsp', -- i don't understand yet...
         },
     },
     { -- mason
-        "williamboman/mason.nvim", dependencies = {
-            "williamboman/mason-lspconfig.nvim",
+        "williamboman/mason.nvim", dependencies = { -- mason lsp
+            "williamboman/mason-lspconfig.nvim", -- gives mason lsp en ensure_installed turple
         },
     },
     { -- lspconfig
-        "neovim/nvim-lspconfig",
+        "neovim/nvim-lspconfig", -- all lsp config in this plugin's config file
     },
     { -- lsp code action ui
-        "nvim-telescope/telescope-ui-select.nvim"
+        "nvim-telescope/telescope-ui-select.nvim" -- better ui for lsp code action
     },
     { -- auto pairs
-        "windwp/nvim-autopairs"
+        "windwp/nvim-autopairs" -- auto complete your bracket
     },
 }
