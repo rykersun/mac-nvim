1c71d631bf9ed7622031093d65e313f0804e5f6f
ryker <ryker@atlantic>
2024-04-07 15:27:28 +0800

    update outline

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,15 +1,21 @@
+local icon = false
+
 return {
     "hedyhli/outline.nvim",
     config = function ()
-        require("outline").setup {
-            symbols = {
-              -- disable all icons
-              -- icon_fetcher = function(_) return "" end,
+        if not icon then
+            require("outline").setup {
+                symbols = {
+                  -- disable all icons
+                  -- icon_fetcher = function(_) return "" end,
 
-              -- icons -> pure text
-              icon_fetcher = function(kind) return kind:sub(1,1) end,
+                  -- icons -> pure text
+                  icon_fetcher = function(kind) return kind:sub(1,1) end,
+                }
             }
-        }
+        else
+            require("outline").setup {}
+        end
         vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
     end
 }
